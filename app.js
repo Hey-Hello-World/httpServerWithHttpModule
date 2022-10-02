c706011d14d53b08bc73849f7e10e167a5f3646c
김성훈 <m1_air@M1air.local>
2022-10-02 16:42:51 +0900

    [ADD] 게시물 등록(생성) 엔드포인트

--- app.js
+++ app.js
@@ -1,19 +1,48 @@
 const http = require('http');
 const server = http.createServer();
 
-const users = [];
-const posts = [];
+const users = [ {
+    id: 1,
+    name: "Rebekah Johnson",
+    email: "Glover12345@gmail.com",
+    password: "123qwe",
+  },
+  {
+    id: 2,
+    name: "Fabian Predovic",
+    email: "Connell29@gmail.com",
+    password: "password",
+  },];
+  
+const posts = [{
+    id: 1,
+    title: "간단한 HTTP API 개발 시작!",
+    content: "Node.js에 내장되어 있는 http 모듈을 사용해서 HTTP server를 구현.",
+    userId: 1,
+  },
+  {
+    id: 2,
+    title: "HTTP의 특성",
+    content: "Request/Response와 Stateless!!",
+    userId: 1,
+  },];
 
 const httpRequestListener = function(request, response){
   const {url, method} = request;
 
   if(method ==='GET'){
+
     if(url === '/users'){
       response.writeHead(200, {'Content-Type': 'application/json'});
       response.end(JSON.stringify({user: users}));
     }
+    else if(url === '/posts'){
+        response.writeHead(200, {'Content-Type': 'application/json'});
+      response.end(JSON.stringify({post: posts}));
+    }
   }
   else if(method === 'POST'){
+
     if(url === '/users'){
 
       let body = '';
@@ -35,6 +64,24 @@ const httpRequestListener = function(request, response){
       response.end(JSON.stringify({message : "userCreated"}));
 
     }
+
+    else if(url === '/posts'){
+        let body ='';
+        request.on('data', data => body+=data);
+        request.on('end', ()=>{
+            const post = JSON.parse(body);
+
+            posts.push({
+                id: post.id,
+                title: post.title,
+                content: post.content,
+                userId: post.userId,
+            });
+        })
+        response.writeHead(201, {'Content-Type': 'application/json'});
+        response.end(JSON.stringify({message : "postCreated"}))
+    }
+
   }
 }
 
