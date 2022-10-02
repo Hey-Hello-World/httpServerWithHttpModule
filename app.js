dea12d4d965523767c7a04eaa35f528538c35d13
김성훈 <m1_air@M1air.local>
2022-10-02 16:55:14 +0900

    [ADD] 게시물 등록(생성) 엔드포인트

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const users = [ {
     email: "Connell29@gmail.com",
     password: "password",
   },];
-
+  
 const posts = [{
     id: 1,
     title: "간단한 HTTP API 개발 시작!",
@@ -35,6 +35,10 @@ const httpRequestListener = function(request, response){
     if(url === '/users'){
       response.writeHead(200, {'Content-Type': 'application/json'});
       response.end(JSON.stringify({user: users}));
+    }
+    else if(url === '/posts'){
+        response.writeHead(200, {'Content-Type': 'application/json'});
+      response.end(JSON.stringify({post: posts}));
     }
   }
   else if(method === 'POST'){
@@ -60,6 +64,24 @@ const httpRequestListener = function(request, response){
       response.end(JSON.stringify({message : "userCreated"}));
 
     }
+
+    else if(url === '/posts'){
+        let body ='';
+        request.on('data', data => body+=data);
+        request.on('end', ()=>{
+            const post = JSON.parse(body);
+
+            posts.push({
+                id: post.id,
+                title: post.title,
+                content: post.content,
+                userId: post.userId,
+            });
+        })
+        response.writeHead(201, {'Content-Type': 'application/json'});
+        response.end(JSON.stringify({message : "postCreated"}))
+    }
+
   }
 }
 
